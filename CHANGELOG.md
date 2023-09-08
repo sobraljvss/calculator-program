a1e64ff6d9ccff04841f69a00aaf503a30a579c3
João Vitor Sobral <sobraljvss@gmail.com>
2023-09-07 21:56:32 -0300

    Added better style and main panel functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,15 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ## [Unreleased]
 
-# [2.0.0] - 2023-09-08
+### Updated
 
-### Added
+- Unique style for fixed tool buttons
+- Background colors change over time
+- Keyboard keys now work too
+
+# [2.0.0] - 2023-09-07
+
+### Updated
 
 - More functionalities (parenthesis, equals, etc)
 - Much better style with Bootstrap
@@ -18,7 +24,7 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 - Single keyboard panel
 - Uniform style for all buttons
 
-## [1.1.0] - 2023-09-07
+## [1.1.0](https://github.com/sobraljvss/calculator-program/compare/64f4e5d...d4682eb567a6cf104408273fd01aa974a08fbc07) - 2023-09-07
 
 ### Added
 
@@ -26,7 +32,7 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 - Minimal structure and style
 - Basic functionalities (it's possible to insert numbers)
 
-## [1.0.0] - 2023-09-03
+## [1.0.0](https://github.com/sobraljvss/calculator-program/commit/64f4e5dd0291cfb9b8cb7b3713843b3886d7f406) - 2023-09-03
 
 ### Added
 
--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 let calc = '';
+let numbers = Array.from(Array(10).keys()).map(num => String(num));
+let operators = Array.from($('.operator')).map(button => button.value);
 
 // Clear screen on reload
 $('#area').text('');
 
 // Number buttons
 Array.from($('.number')).forEach(button => button.addEventListener('click', () => calc += button.value));
+
+// Operator buttons
+Array.from($('.operator')).forEach(button => button.addEventListener('click', () => {
+  if (operators.includes(calc.charAt(calc.length-1))) {calc = calc.slice(0, -1);}
+  if (calc.charAt(calc.length-1) == '.') {calc += '0';}
+  calc += button.value;
+}));
+
+// Point button
+$('#point').click(() => {
+  if (!numbers.includes(calc.charAt(calc.length-1))) {calc += 0;}
+  calc += '.';
+});
+
+// Equals button
+$('#equals').click(() => {
+  try {
+    if (Array.from(calc.matchAll(/[(]/g)).length > Array.from(calc.matchAll(/[)]/g)).length) {calc += ')';} else if (Array.from(calc.matchAll(/[)]/g)).length > Array.from(calc.matchAll(/[(]/g)).length) {calc = '(' + calc;}
+    calc = String(eval(calc));
+  } catch (SyntaxError) {
+    calc = calc.slice(1);
+  }
+});
+
+// TODO: test parenthesis
+
+// Clear and Backspace buttons
+$('#clear').click(() => calc = '');
+$('#backspace').click(() => calc = calc.slice(0, -1));
 
 // All buttons update screen
 Array.from($('button')).forEach(button => button.addEventListener('click', () => $('#screen').text(calc)));
