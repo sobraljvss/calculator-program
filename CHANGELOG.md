ac0eb610de351520e409598628639a41721986cd
João Vitor Sobral <sobraljvss@gmail.com>
2023-09-16 10:59:26 -0300

    Turned keyboard buttons usable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,28 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ## [Unreleased]
 
+### Added
+
+- [MathJS](https://mathjs.org/) implemented
+- New panel with more advanced functionalities
+- Carousel for switching panels
+
+### Fixed
+
+- Now expressions are displayed in mathematics syntax
+- Expressions are evaluated with MathJS
+
+# [2.1.0] - 2023-09-16
+
 ### Updated
 
 - Unique style for fixed tool buttons
-- Background colors change over time
 - Keyboard keys now work too
+
+### Deprecated
+
+- Evaluation of expressions via eval function
+- Expressions displayed in JavaScript syntax
 
 # [2.0.0] - 2023-09-07
 
@@ -24,15 +41,15 @@ This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 - Single keyboard panel
 - Uniform style for all buttons
 
-## [1.1.0](https://github.com/sobraljvss/calculator-program/compare/64f4e5d...d4682eb567a6cf104408273fd01aa974a08fbc07) - 2023-09-07
+## [1.1.0] - 2023-09-07
 
 ### Added
 
-- Implemented Bootstrap and jQuery
+- Implemented [Bootstrap](https://getbootstrap.com/) and [jQuery](https://jquery.com/)
 - Minimal structure and style
 - Basic functionalities (it's possible to insert numbers)
 
-## [1.0.0](https://github.com/sobraljvss/calculator-program/commit/64f4e5dd0291cfb9b8cb7b3713843b3886d7f406) - 2023-09-03
+## [1.0.0] - 2023-09-03
 
 ### Added
 
--- script.js
+++ script.js
@@ -5,11 +5,12 @@ let operators = Array.from($('.operator')).map(button => button.value);
 // Clear screen on reload
 $('#area').text('');
 
-// Number buttons
-Array.from($('.number')).forEach(button => button.addEventListener('click', () => calc += button.value));
+// Numbers and parenthesis buttons
+Array.from($('.numbers')).forEach(button => button.addEventListener('click', () => calc += button.value));
+Array.from($('.parenthesis')).forEach(button => button.addEventListener('click', () => calc += button.value));
 
-// Operator buttons
-Array.from($('.operator')).forEach(button => button.addEventListener('click', () => {
+// Operators buttons
+Array.from($('.operators')).forEach(button => button.addEventListener('click', () => {
   if (operators.includes(calc.charAt(calc.length-1))) {calc = calc.slice(0, -1);}
   if (calc.charAt(calc.length-1) == '.') {calc += '0';}
   calc += button.value;
@@ -31,8 +32,6 @@ $('#equals').click(() => {
   }
 });
 
-// TODO: test parenthesis
-
 // Clear and Backspace buttons
 $('#clear').click(() => calc = '');
 $('#backspace').click(() => calc = calc.slice(0, -1));
@@ -40,3 +39,18 @@ $('#backspace').click(() => calc = calc.slice(0, -1));
 // All buttons update screen
 Array.from($('button')).forEach(button => button.addEventListener('click', () => $('#screen').text(calc)));
 
+// Make keyboard buttons usable
+window.addEventListener('keydown', event => {
+  if (event.key == '/' || event.key == 'Enter') {event.preventDefault();}
+  else if (event.key == ',') {$('#point').click();}
+  try{
+    if (event.key != 'Shift') {
+      Array.from($('button')).filter(button => button.getAttribute('key') == event.key)[0].click();
+    }
+  } catch (TypeError) {} // Ignores keys with no matching button
+});
+
+// Change color with reload [TEST]
+/*$('body').css('background', `radial-gradient(rgb(${color.r}, ${color.g}, ${color.b}), rgb(${comp_color.r}, ${comp_color.g}, ${comp_color.b}))`);
+$('#tools > button').css('background-color', `rgba(${comp_color.r}, ${comp_color.g}, ${comp_color.b}, 0.5)`);
+$('#tools > button').mouseenter(() => $(this).css('background-color', `rgba(${comp_color.r}, ${comp_color.g}, ${comp_color.b}, 0.7)`));*/
